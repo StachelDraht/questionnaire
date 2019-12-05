f332307f508327dc6c5aa3ba49be07bde7c9d111
Maksim Karpenko <maksim.karpenko@gmail.com>
2019-12-05 11:58:40 +0200

    user model password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,22 @@
 const mongoos = require('mongoose')
 const Schema = mongoos.Schema
+const bcrypt = require('bcryptjs')
 
 let UserSchema = new Schema({
     username: {type: String, require: true, unique: true},
     email: {type: String, require: true, unique: true},
     password: {type: String, require: true}
+})
+
+UserSchema.pre('save', function(next) {
+    var user = this
+    if(!user.isModified('password')) {return next()}
+    bcrypt.hash(user.password,10).then(hashedpassword => {
+        user.password = hashedpassword
+        next()
+    })
+}, function(err) {
+    next(err)
 })
 
 module.exports = mongoos.model('User', UserSchema)
