df7bae7c38e5a2f6f4b62bc74f7f10456f037829
Maksim Karpenko <maksim.karpenko@ericsson.com>
2019-12-11 14:30:23 +0200

    router added to project

--- frontend_questionnaire/src/App.vue
+++ frontend_questionnaire/src/App.vue
@@ -58,7 +58,6 @@
                     name="login"
                     type="text"
                   />
-
                   <v-text-field
                     id="password"
                     label="Password"
@@ -88,21 +87,17 @@
                   </div>
                   <!-- end -->
                   <!--<v-stepper-step :complete="e6 > 2" step="2">Configure analytics for this app</v-stepper-step>
-
                   <v-stepper-content step="2">
                     <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
                     <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
                     <v-btn text>Cancel</v-btn>
                   </v-stepper-content>
-
                   <v-stepper-step :complete="e6 > 3" step="3">Select an ad format and name ad unit</v-stepper-step>
-
                   <v-stepper-content step="3">
                     <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
                     <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
                     <v-btn text>Cancel</v-btn>
                   </v-stepper-content>
-
                   <v-stepper-step step="4">View setup instructions</v-stepper-step>
                   <v-stepper-content step="4">
                     <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
@@ -133,7 +128,6 @@
   import typetext from './components/typeText'
   import typerate from './components/typeRate.vue'
   import {api} from './data/axios.base'
-
   export default {
     data() {
       return {
